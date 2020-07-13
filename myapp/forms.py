08fb5d70c61ae4f6918a29c474c0bcb417486b68
deepak <nvenkatadeepak@gmail.com>
2020-07-13 15:15:59 -0400

    lab7 part 1 completed

---
+++ myapp/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from myapp.models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('student', 'course', 'levels', 'order_date')
+        widgets= {
+            'student':forms.RadioSelect(attrs={'class':'radio'}),
+            'order_date':forms.SelectDateWidget(attrs={'class':'years=date.today()'}),
+        }
+        labels ={
+            'student': "Student Name",
+            'order_date': "Order Date",
+        }
+class InterestForm(forms.Form):
+     CHOICES= (('1', 'Yes',),('0', 'No',))
+     interested= forms.CharField(widget=forms.RadioSelect(choices=CHOICES))
+     levels= forms.IntegerField(initial=1)
+     comments= forms.ChatField(widget=forms.Textarea(), required =False, label ="Additional Comments")
