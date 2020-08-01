8c633eaa9926aa19617356a065a070ed16011f53
Gundeep Singh <gundeep5830@gmail.com>
2020-08-01 18:47:15 -0400

    All completed

--- mysite20/myapp/views.py
+++ mysite20/myapp/views.py
@@ -1,13 +1,17 @@
 # Import necessary classes
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import Topic, Course, Student, Order
+from json import dumps
+import datetime
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django.shortcuts import render
 from .forms import OrderForm,InterestForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.core.serializers.json import DjangoJSONEncoder
 # Create your views here.
+
 
 
 def index(request):
@@ -32,7 +36,18 @@ def index(request):
     #     para1 = '<p>' + str(course.name) + ':' + forEveryone + '</p>'
     #     response.write(para1)
 
-    return render(request, 'myapp/index.html', {'top_list': top_list})
+    if request.session.test_cookie_worked():
+        print('The test cookie is working')
+        request.session.delete_test_cookie()
+        print('Test cookie Deleted also')
+
+    last_logged = ''
+    if request.session.has_key('last_connection'):
+        last_logged = request.session['last_connection']
+        print(last_logged)
+
+
+    return render(request, 'myapp/index.html', {'top_list': top_list, 'last_logged': last_logged})
 
     '''Part 1
     2. C) Wr are passing the context variable 'top_list' to the index view
@@ -40,10 +55,29 @@ def index(request):
 
 
 def about(request):
+
     # response = HttpResponse()
     # response.write('<h1>This is an E-learning Website! Search our Topics to find all available Courses</h1>')
+
     data="This is an E-learning Website! Search our Topics to find all available Courses"
-    return render(request, 'myapp/about.html', {'data': data})
+
+    if request.session.has_key('about_visits'):
+        count = request.session['about_visits']
+        if count >= 1:
+            count = count + 1
+            request.session['about_visits'] = count
+            final1 = count
+            request.session.set_expiry(300)
+    else:
+        request.session['about_visits'] = 1
+        final1 = 1
+        request.session.set_expiry(300)
+
+    response = render(request, 'myapp/about.html', {'data': data, 'visits': final1})
+    # Test cookie
+    request.session.set_test_cookie()
+    return response
+
     '''Part 1
     4. C) Wr are passing the context variable 'data' to the about view
     '''
@@ -80,12 +114,12 @@ def placeOrder(request):
             order = form.save(commit=False)
             if order.levels <= order.course.stages:
 
-                if order.course.price>150:
+                if order.course.price > 150:
                     print("--im here--")
                     discounted_price = order.course.discount()
                     order.course.save()
 
-                    Course.objects.filter(name=str(order.course)).update(price = discounted_price)
+                    Course.objects.filter(name=str(order.course)).update(price=discounted_price)
                     # order.save()
                 msg = 'Your course has been ordered successfully.'
             else:
@@ -112,7 +146,12 @@ def courseDetail(request,cour_id):
         form = InterestForm()
     return render(request, 'myapp/coursedetail.html', {'form': form,'course': course})
 
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
 
+    if isinstance(obj, (datetime.datetime,datetime.datetime.date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
 
 # Create your views here.
 def user_login(request):
@@ -123,7 +162,11 @@ def user_login(request):
         if user:
             if user.is_active:
                 login(request, user)
-                return HttpResponseRedirect(reverse('myapp:index'))
+                response = HttpResponseRedirect(reverse('myapp:index'))
+                request.session['last_connection'] = dumps(datetime.datetime.now(), default=json_serial)
+                request.session.set_expiry(3600)
+                return response
+
             else:
                 return HttpResponse('Your account is disabled.')
         else:
@@ -133,7 +176,8 @@ def user_login(request):
 
 @login_required
 def user_logout(request):
-    logout(request)
+    del request.session['last_connection']
+    # logout(request)
     return HttpResponseRedirect(reverse(('myapp:index')))
 
 @login_required
